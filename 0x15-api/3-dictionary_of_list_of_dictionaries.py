656ccfd376330901972711d8b876adff47d2a631
etojama <torohkiss@gmail.com>
2022-10-17 17:24:21 -0700

    task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    A python script that, using a REST API,
+    returns information about all employeer TODO
+"""
+
+
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    emps = requests.get("https://jsonplaceholder.typicode.com/users").json()
+
+    employees = {}
+    for emp in emps:
+        empId = emp['id']
+        employees.update({empId: []})
+        task_url = "https://jsonplaceholder.typicode.com/users/{}/todos".\
+                format(empId)
+        tasks = requests.get(task_url).json()
+
+        for task in tasks:
+            employees[empId].append({'username': emp['username'],
+                'task': task['title'],
+                'completed': task['completed']})
+
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(employees, f)
